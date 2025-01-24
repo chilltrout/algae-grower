965cce38e42d857513515e7d77741833b9e3f2ca
chilltrout <rbrown92@me.com>
2025-01-25 00:53:22 +1000

    Update turbidity_sensor.h

--- components/turbidity_sensor/turbidity_sensor.h
+++ components/turbidity_sensor/turbidity_sensor.h
@@ -9,6 +9,7 @@ namespace turbidity_sensor {
 
 class TurbiditySensor : public sensor::Sensor, public Component {
  public:
+  void set_uart(uart::UARTComponent *uart) { uart_ = uart; }
   void set_expander(Component *expander) { expander_ = expander; }
   void set_channel(uint8_t channel) { channel_ = channel; }
   
@@ -17,6 +18,7 @@ class TurbiditySensor : public sensor::Sensor, public Component {
   void update() override;
   
  protected:
+  uart::UARTComponent *uart_{nullptr};
   Component *expander_{nullptr};
   uint8_t channel_{0};
 };
