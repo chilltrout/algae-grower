0d4b58eac4dd99cff5e4d3c992a4addbdc91eff6
chilltrout <rbrown92@me.com>
2025-01-25 00:10:20 +1000

    Update turbidity_sensor.h

--- components/turbidity_sensor/turbidity_sensor.h
+++ components/turbidity_sensor/turbidity_sensor.h
@@ -1,39 +1,24 @@
 #pragma once
 
-#include "esphome/core/component.h"
-#include "esphome/components/sensor/sensor.h"
-#include "esphome/core/gpio.h"
+#include "esphome.h"
+#include "../atlas_serial_expander/atlas_serial_expander.h"
 
-namespace esphome {
-namespace turbidity_sensor {
-
-class TurbidityPlatform : public Component {
+class TurbiditySensor : public Component, public Sensor {
  public:
-  void set_s1_pin(GPIOPin *pin) { s1_pin_ = pin; }
-  void set_s2_pin(GPIOPin *pin) { s2_pin_ = pin; }
-  void set_s3_pin(GPIOPin *pin) { s3_pin_ = pin; }
+  TurbiditySensor(UARTComponent *parent, AtlasSerialExpander *expander, uint8_t channel)
+      : uart_(parent), expander_(expander), channel_(channel) {}
 
-  GPIOPin *get_s1_pin() const { return s1_pin_; }
-  GPIOPin *get_s2_pin() const { return s2_pin_; }
-  GPIOPin *get_s3_pin() const { return s3_pin_; }
+  void setup() override {
+    // Initialize the sensor
+  }
 
- private:
-  GPIOPin *s1_pin_;
-  GPIOPin *s2_pin_;
-  GPIOPin *s3_pin_;
-};
-
-class TurbiditySensor : public sensor::Sensor, public Component {
- public:
-  void set_channel(uint8_t channel) { channel_ = channel; }
-  void set_uart_id(UARTComponent *uart) { uart_ = uart; }
-  void set_platform(TurbidityPlatform *platform) { platform_ = platform; }
-
-  void setup() override;
-  void update() override;
+  void update() override {
+    expander_->select_channel(channel_);
+    // Read data from the sensor
+  }
 
  protected:
-  uint8_t channel_;
   UARTComponent *uart_;
-  TurbidityPlatform *platform_;
+  AtlasSerialExpander *expander_;
+  uint8_t channel_;
 };
