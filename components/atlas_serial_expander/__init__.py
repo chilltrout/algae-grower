b980e1d5e5e330ab3c59ab563b4ae88b2ff26dde
chilltrout <rbrown92@me.com>
2025-01-25 01:22:41 +1000

    Update __init__.py

--- components/atlas_serial_expander/__init__.py
+++ components/atlas_serial_expander/__init__.py
@@ -1,6 +1,5 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import gpio
 from esphome.const import CONF_ID
 
 DEPENDENCIES = ['gpio']
@@ -8,23 +7,10 @@ DEPENDENCIES = ['gpio']
 atlas_serial_expander_ns = cg.esphome_ns.namespace('atlas_serial_expander')
 AtlasSerialExpander = atlas_serial_expander_ns.class_('AtlasSerialExpander', cg.Component)
 
-CONF_S1_PIN = 's1_pin'
-CONF_S2_PIN = 's2_pin'
-CONF_S3_PIN = 's3_pin'
-
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(AtlasSerialExpander),
-    cv.Required(CONF_S1_PIN): cv.int_,
-    cv.Required(CONF_S2_PIN): cv.int_,
-    cv.Required(CONF_S3_PIN): cv.int_,
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
-    s1 = await cg.gpio_pin_expression(config[CONF_S1_PIN])
-    s2 = await cg.gpio_pin_expression(config[CONF_S2_PIN])
-    s3 = await cg.gpio_pin_expression(config[CONF_S3_PIN])
-
-    cg.add(var.set_pins(s1, s2, s3))
