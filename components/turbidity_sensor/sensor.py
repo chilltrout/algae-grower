7642845e39d708626f5a778b9bc2877169630a8f
chilltrout <rbrown92@me.com>
2025-01-25 00:30:38 +1000

    Update sensor.py

--- components/turbidity_sensor/sensor.py
+++ components/turbidity_sensor/sensor.py
@@ -21,7 +21,7 @@ CONFIG_SCHEMA = sensor.sensor_schema().extend({
 }).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = await cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await sensor.register_sensor(var, config)
     await uart.register_uart_device(var, config)
