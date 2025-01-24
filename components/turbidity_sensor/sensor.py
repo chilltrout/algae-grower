c96df3150959df45102decd79f6ae77854dd71c5
chilltrout <rbrown92@me.com>
2025-01-25 00:28:12 +1000

    Update sensor.py

--- components/turbidity_sensor/sensor.py
+++ components/turbidity_sensor/sensor.py
@@ -1,56 +1,31 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, uart
-from esphome.const import CONF_ID, CONF_NAME, CONF_CHANNEL, CONF_UART_ID
+from esphome.const import CONF_ID, CONF_CHANNEL
 
 DEPENDENCIES = ['uart']
+AUTO_LOAD = ['sensor']
 
-turbidity_ns = cg.esphome_ns.namespace('turbidity_sensor')
-TurbiditySensor = turbidity_ns.class_('TurbiditySensor', sensor.Sensor, cg.Component)
-TurbidityPlatform = turbidity_ns.class_('TurbidityPlatform', cg.Component)
+CONF_EXPANDER_ID = 'expander_id'
 
-CONF_S1_PIN = 's1_pin'
-CONF_S2_PIN = 's2_pin'
-CONF_S3_PIN = 's3_pin'
+turbidity_sensor_ns = cg.esphome_ns.namespace('turbidity_sensor')
+TurbiditySensor = turbidity_sensor_ns.class_('TurbiditySensor', cg.Component, sensor.Sensor)
 
-turbidity_platform_schema = cv.Schema({
-    cv.Required(CONF_ID): cv.declare_id(TurbidityPlatform),
-    cv.Required(CONF_S1_PIN): cv.gpio_pin_schema,
-    cv.Required(CONF_S2_PIN): cv.gpio_pin_schema,
-    cv.Required(CONF_S3_PIN): cv.gpio_pin_schema,
-})
+atlas_serial_expander_ns = cg.esphome_ns.namespace('atlas_serial_expander')
+AtlasSerialExpander = atlas_serial_expander_ns.class_('AtlasSerialExpander', cg.Component)
 
 CONFIG_SCHEMA = sensor.sensor_schema().extend({
     cv.GenerateID(): cv.declare_id(TurbiditySensor),
-    cv.Required(CONF_NAME): cv.string,
     cv.Required(CONF_CHANNEL): cv.int_range(min=0, max=7),
-    cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-}).extend(cv.COMPONENT_SCHEMA)
+    cv.Required(CONF_EXPANDER_ID): cv.use_id(AtlasSerialExpander),
+}).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config[CONF_NAME])
-    yield cg.register_component(var, config)
-    yield sensor.register_sensor(var, config)
-
-    platform = cg.global_ns.get('turbidity_platform')
-    var.set_platform(platform)
-
-    uart_component = yield cg.get_variable(config[CONF_UART_ID])
-    var.set_uart_id(uart_component)
-
-    var.set_channel(config[CONF_CHANNEL])
-
-def turbidity_platform_to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
+    await cg.register_component(var, config)
+    await sensor.register_sensor(var, config)
+    await uart.register_uart_device(var, config)
 
-    s1_pin = yield cg.gpio_pin_expression(config[CONF_S1_PIN])
-    var.set_s1_pin(s1_pin)
-
-    s2_pin = yield cg.gpio_pin_expression(config[CONF_S2_PIN])
-    var.set_s2_pin(s2_pin)
-
-    s3_pin = yield cg.gpio_pin_expression(config[CONF_S3_PIN])
-    var.set_s3_pin(s3_pin)
-
-    cg.add_global(turbidity_ns.namespace().set_platform(var))
+    expander = await cg.get_variable(config[CONF_EXPANDER_ID])
+    cg.add(var.set_expander(expander))
+    cg.add(var.set_channel(config[CONF_CHANNEL]))
