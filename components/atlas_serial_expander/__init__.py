6101d317f5ae24ec82314ac2c3ba7d45da1ca044
chilltrout <rbrown92@me.com>
2025-01-25 01:07:48 +1000

    Update __init__.py

--- components/atlas_serial_expander/__init__.py
+++ components/atlas_serial_expander/__init__.py
@@ -14,17 +14,17 @@ CONF_S3_PIN = 's3_pin'
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(AtlasSerialExpander),
-    cv.Required(CONF_S1_PIN): cv.All(cv.int_, cv.Range(min=0)),
-    cv.Required(CONF_S2_PIN): cv.All(cv.int_, cv.Range(min=0)),
-    cv.Required(CONF_S3_PIN): cv.All(cv.int_, cv.Range(min=0)),
+    cv.Required('s1_pin'): cv.All(cv.int_, cv.Range(min=0)),
+    cv.Required('s2_pin'): cv.All(cv.int_, cv.Range(min=0)),
+    cv.Required('s3_pin'): cv.All(cv.int_, cv.Range(min=0)),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    s1 = await cg.gpio_pin_expression(config[CONF_S1_PIN])
-    s2 = await cg.gpio_pin_expression(config[CONF_S2_PIN])
-    s3 = await cg.gpio_pin_expression(config[CONF_S3_PIN])
+    s1 = await cg.gpio_pin_expression(config['s1_pin'])
+    s2 = await cg.gpio_pin_expression(config['s2_pin'])
+    s3 = await cg.gpio_pin_expression(config['s3_pin'])
 
     cg.add(var.set_pins(s1, s2, s3))
