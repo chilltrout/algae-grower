7efe099a9075049b770ac50953f4c16448721aa0
chilltrout <rbrown92@me.com>
2025-01-25 00:15:46 +1000

    Update __init__.py

--- components/atlas_serial_expander/__init__.py
+++ components/atlas_serial_expander/__init__.py
@@ -1,3 +1,7 @@
+LLM served by Perplexity Labs
+I see the issue now. The error is occurring because the gpio_output_pin_schema is not defined in the esphome.components.gpio module. This is likely due to changes in the ESPHome API. Let's modify the atlas_serial_expander component to use the correct pin schema. Here's how we can fix this:
+Update the __init__.py file in your atlas_serial_expander component:
+python
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import gpio
@@ -10,17 +14,17 @@ AtlasSerialExpander = atlas_serial_expander_ns.class_('AtlasSerialExpander', cg.
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(AtlasSerialExpander),
-    cv.Required('s1_pin'): gpio.gpio_output_pin_schema,
-    cv.Required('s2_pin'): gpio.gpio_output_pin_schema,
-    cv.Required('s3_pin'): gpio.gpio_output_pin_schema,
+    cv.Required('s1_pin'): cv.All(cv.Required, cv.pins_schema),
+    cv.Required('s2_pin'): cv.All(cv.Required, cv.pins_schema),
+    cv.Required('s3_pin'): cv.All(cv.Required, cv.pins_schema),
 }).extend(cv.COMPONENT_SCHEMA)
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
+    await cg.register_component(var, config)
 
-    s1 = yield gpio.setup_gpio_output_pin(config['s1_pin'])
-    s2 = yield gpio.setup_gpio_output_pin(config['s2_pin'])
-    s3 = yield gpio.setup_gpio_output_pin(config['s3_pin'])
+    s1 = await gpio.setup_pin(config['s1_pin'], cv.PinMode.OUTPUT)
+    s2 = await gpio.setup_pin(config['s2_pin'], cv.PinMode.OUTPUT)
+    s3 = await gpio.setup_pin(config['s3_pin'], cv.PinMode.OUTPUT)
 
     cg.add(var.set_pins(s1, s2, s3))
