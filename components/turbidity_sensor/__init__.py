592463ba4b8128082bceb2250de7127e2053e0ba
chilltrout <rbrown92@me.com>
2025-01-25 00:31:51 +1000

    Update __init__.py

--- components/turbidity_sensor/__init__.py
+++ components/turbidity_sensor/__init__.py
@@ -1,7 +1,14 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID
 from . import sensor as turbidity_sensor_sensor
 
+DEPENDENCIES = ['uart']
+AUTO_LOAD = ['sensor']
+
 CONFIG_SCHEMA = turbidity_sensor_sensor.CONFIG_SCHEMA
 DEPENDENCIES = turbidity_sensor_sensor.DEPENDENCIES
-async_setup_entry = sensor.sensor_async_setup_entry
+
+async def to_code(config):
+    await sensor.register_sensor(config)
