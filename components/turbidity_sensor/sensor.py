dcfd2fd49e6bc7ad6cacb4e186437e936bb49e51
chilltrout <rbrown92@me.com>
2025-01-24 23:03:48 +1000

    Update sensor.py

--- components/turbidity_sensor/sensor.py
+++ components/turbidity_sensor/sensor.py
@@ -1,5 +1,6 @@
 import esphome.config_validation as cv
 import esphome.codegen as cg
+from esphome import pins
 from esphome.components import uart, sensor
 from esphome.const import CONF_ID, CONF_NAME
 
@@ -11,9 +12,9 @@ CONFIG_SCHEMA = (
     .extend(
         {
             cv.Required(CONF_ID): cv.declare_id(TurbiditySensor),
-            cv.Required('s1_pin'): cv.gpio_pin,  # Correct method for GPIO pin
-            cv.Required('s2_pin'): cv.gpio_pin,
-            cv.Required('s3_pin'): cv.gpio_pin,
+            cv.Required('s1_pin'): pins.gpio_output_pin_schema,
+            cv.Required('s2_pin'): pins.gpio_output_pin_schema,
+            cv.Required('s3_pin'): pins.gpio_output_pin_schema,
             cv.Optional(CONF_NAME, default="Turbidity Sensor"): cv.string,
         }
     )
@@ -25,10 +26,15 @@ def to_code(config):
     yield cg.register_component(var, config)
     yield sensor.register_sensor(var, config)
 
-    # Assume these methods exist in your C++ code to set the pins
-    cg.add(var.set_s1_pin(config['s1_pin']))
-    cg.add(var.set_s2_pin(config['s2_pin']))
-    cg.add(var.set_s3_pin(config['s3_pin']))
+    # Assume these methods exist in your C++ code
+    s1_pin = yield cg.gpio_pin_expression(config['s1_pin'])
+    cg.add(var.set_s1_pin(s1_pin))
+    
+    s2_pin = yield cg.gpio_pin_expression(config['s2_pin'])
+    cg.add(var.set_s2_pin(s2_pin))
+    
+    s3_pin = yield cg.gpio_pin_expression(config['s3_pin'])
+    cg.add(var.set_s3_pin(s3_pin))
 
     uart_var = yield cg.get_variable(config[uart.CONF_UART_ID])
     cg.add(var.set_uart(uart_var))
