a4a04a78f5f9b9081bb12d25711315f1ad1021e0
chilltrout <rbrown92@me.com>
2025-01-25 01:26:22 +1000

    Update atlas_serial_expander.h

--- components/atlas_serial_expander/atlas_serial_expander.h
+++ components/atlas_serial_expander/atlas_serial_expander.h
@@ -1,15 +1,28 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/gpio/gpio.h"
+#include "esphome/core/gpio.h"
 
 namespace esphome {
 namespace atlas_serial_expander {
 
 class AtlasSerialExpander : public Component {
  public:
+  void set_pins(GPIOPin *s1, GPIOPin *s2, GPIOPin *s3) {
+    s1_pin_ = s1;
+    s2_pin_ = s2;
+    s3_pin_ = s3;
+  }
+
   void setup() override;
   void loop() override;
+
+  void select_channel(uint8_t channel);
+
+ protected:
+  GPIOPin *s1_pin_;
+  GPIOPin *s2_pin_;
+  GPIOPin *s3_pin_;
 };
 
 }  // namespace atlas_serial_expander
