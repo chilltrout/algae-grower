e5a2f5ad06193650edbe48c5b138ff316b514246
chilltrout <rbrown92@me.com>
2025-01-25 00:23:01 +1000

    Update turbidity_sensor.cpp

--- components/turbidity_sensor/turbidity_sensor.cpp
+++ components/turbidity_sensor/turbidity_sensor.cpp
@@ -1,27 +1,9 @@
 #include "turbidity_sensor.h"
 
-void TurbiditySensor::setup() {
-  // Any setup code needed for the turbidity sensor
-}
+namespace esphome {
+namespace turbidity_sensor {
 
-void TurbiditySensor::update() {
-  expander_->select_channel(channel_);
-  
-  // Read data from the sensor via UART
-  // This is a placeholder. You'll need to implement the actual communication protocol
-  // with your turbidity sensor here.
-  
-  char buffer[32];
-  if (uart_->read_line(buffer, sizeof(buffer))) {
-    float value = atof(buffer);
-    publish_state(value);
-  }
-}
+// If you need to implement any complex functions, do it here
 
-void TurbiditySensor::set_expander(AtlasSerialExpander *expander) {
-  expander_ = expander;
-}
-
-void TurbiditySensor::set_channel(uint8_t channel) {
-  channel_ = channel;
-}
+}  // namespace turbidity_sensor
+}  // namespace esphome
