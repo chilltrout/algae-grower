fb0975695088e98f1d384cc69d1def822f42175a
chilltrout <rbrown92@me.com>
2025-01-24 22:35:09 +1000

    Update turbidity_sensor.cpp

--- components/turbidity_sensor/turbidity_sensor.cpp
+++ components/turbidity_sensor/turbidity_sensor.cpp
@@ -1,66 +1,61 @@
-#include "esphome.h"
+#include "turbidity_sensor.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace turbidity_sensor {
 
-class TurbiditySensor : public sensor::Sensor, public PollingComponent {
- public:
-  TurbiditySensor(GPIOPin *s1_pin, GPIOPin *s2_pin, GPIOPin *s3_pin, UARTComponent *uart, int channel)
-      : PollingComponent(10000), s1_pin_(s1_pin), s2_pin_(s2_pin), s3_pin_(s3_pin), uart_bus_(uart), channel_(channel) {}
+static const char *TAG = "turbidity_sensor";
 
-  void setup() override {
-    ESP_LOGCONFIG("TurbiditySensor", "Setting up Turbidity Sensor on channel %d", channel_);
+void TurbiditySensor::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up Turbidity Sensor on channel %u...", this->channel_);
+  // Additional setup code if necessary
+}
+
+void TurbiditySensor::update() {
+  ESP_LOGD(TAG, "Updating Turbidity Sensor on channel %u...", this->channel_);
+
+  // Example: Set the pins according to the channel
+  this->platform_->get_s1_pin()->digital_write((this->channel_ & 0x01) != 0);
+  this->platform_->get_s2_pin()->digital_write((this->channel_ & 0x02) != 0);
+  this->platform_->get_s3_pin()->digital_write((this->channel_ & 0x04) != 0);
+
+  // Wait for the multiplexer to switch channels
+  delay(10);
+
+  // Read data from the sensor via UART
+  // Ensure the UART buffer is clear
+  while (this->uart_->available()) {
+    this->uart_->read();
   }
 
-  void update() override {
-    // Set the channel on the serial port expander
-    this->s1_pin_->digital_write((channel_ & 0x01) > 0);
-    this->s2_pin_->digital_write((channel_ & 0x02) > 0);
-    this->s3_pin_->digital_write((channel_ & 0x04) > 0);
+  // Send a command to the sensor to request data
+  const char *command = "R";
+  this->uart_->write_str(command);
+  this->uart_->write_str("\r");
 
-    // Small delay to stabilize switching
-    delay(100);
+  // Wait for the sensor to respond
+  delay(100);
 
-    // Send the request command to the sensor
-    uint8_t command[] = {0x18, 0x05, 0x00, 0x01, 0x0D};
-    this->uart_bus_->write_array(command, sizeof(command));
-
-    // Wait for a response
-    delay(100);
-
-    // Read the response
-    std::vector<uint8_t> response;
-    while (this->uart_bus_->available()) {
-      response.push_back(this->uart_bus_->read());
+  // Read the response
+  String response;
+  while (this->uart_->available()) {
+    char c = this->uart_->read();
+    if (c == '\r') {
+      break;
     }
-
-    // Validate and parse the response
-    if (response.size() >= 5 && response[0] == 0x18 && response[1] == 0x05) {
-      float turbidity_value = static_cast<float>(response[3]);
-      ESP_LOGD("TurbiditySensor", "Channel %d: Turbidity value = %.2f", channel_, turbidity_value);
-      publish_state(turbidity_value);
-    } else {
-      ESP_LOGW("TurbiditySensor", "Invalid response on channel %d", channel_);
-      publish_state(NAN);
-    }
+    response += c;
   }
 
-  void dump_config() override {
-    ESP_LOGCONFIG("TurbiditySensor", "Turbidity Sensor:");
-    ESP_LOGCONFIG("TurbiditySensor", "  Channel: %d", channel_);
-    LOG_PIN("  S1 Pin: ", s1_pin_);
-    LOG_PIN("  S2 Pin: ", s2_pin_);
-    LOG_PIN("  S3 Pin: ", s3_pin_);
-    LOG_UART("  UART Bus: ", uart_bus_);
+  // Parse the response
+  float turbidity = response.toFloat();
+  if (turbidity == 0 && response != "0.0") {
+    ESP_LOGW(TAG, "Failed to read turbidity data");
+    return;
   }
 
- protected:
-  GPIOPin *s1_pin_;
-  GPIOPin *s2_pin_;
-  GPIOPin *s3_pin_;
-  UARTComponent *uart_bus_;
-  int channel_;
-};
+  // Publish the turbidity value
+  this->publish_state(turbidity);
+}
 
 }  // namespace turbidity_sensor
 }  // namespace esphome
