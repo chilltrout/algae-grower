3aa3d8ee00fea5c1db8e4d5618626d8d62ffdb6e
chilltrout <rbrown92@me.com>
2025-01-25 00:22:34 +1000

    Update turbidity_sensor.h

--- components/turbidity_sensor/turbidity_sensor.h
+++ components/turbidity_sensor/turbidity_sensor.h
@@ -1,24 +1,28 @@
-#pragma once
-
-#include "esphome.h"
-#include "../atlas_serial_expander/atlas_serial_expander.h"
-
-class TurbiditySensor : public Component, public Sensor {
- public:
-  TurbiditySensor(UARTComponent *parent, AtlasSerialExpander *expander, uint8_t channel)
-      : uart_(parent), expander_(expander), channel_(channel) {}
-
-  void setup() override {
-    // Initialize the sensor
+ void setup() override {
+    // Any setup code needed for the turbidity sensor
   }
 
   void update() override {
     expander_->select_channel(channel_);
-    // Read data from the sensor
+    
+    // Read data from the sensor via UART
+    // This is a placeholder. You'll need to implement the actual communication protocol
+    // with your turbidity sensor here.
+    
+    char buffer[32];
+    if (available()) {
+      size_t bytes_read = read_line(buffer, sizeof(buffer));
+      if (bytes_read > 0) {
+        float value = atof(buffer);
+        publish_state(value);
+      }
+    }
   }
 
  protected:
-  UARTComponent *uart_;
-  AtlasSerialExpander *expander_;
+  atlas_serial_expander::AtlasSerialExpander *expander_;
   uint8_t channel_;
 };
+
+}  // namespace turbidity_sensor
+}  // namespace esphome
