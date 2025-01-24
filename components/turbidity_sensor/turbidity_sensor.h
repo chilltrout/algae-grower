82953ea1276d277967a37953a13d6b4dec2cdf3a
chilltrout <rbrown92@me.com>
2025-01-25 01:49:56 +1000

    Update turbidity_sensor.h

--- components/turbidity_sensor/turbidity_sensor.h
+++ components/turbidity_sensor/turbidity_sensor.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/uart/uart.h"
+#include "../atlas_serial_expander/atlas_serial_expander.h"
 
 namespace esphome {
 namespace turbidity_sensor {
@@ -10,7 +11,7 @@ namespace turbidity_sensor {
 class TurbiditySensor : public sensor::Sensor, public Component {
  public:
   void set_uart(uart::UARTComponent *uart) { uart_ = uart; }
-  void set_expander(Component *expander) { expander_ = expander; }
+  void set_expander(atlas_serial_expander::AtlasSerialExpander *expander) { expander_ = expander; }
   void set_channel(uint8_t channel) { channel_ = channel; }
   
   void setup() override;
@@ -19,7 +20,7 @@ class TurbiditySensor : public sensor::Sensor, public Component {
   
  protected:
   uart::UARTComponent *uart_{nullptr};
-  Component *expander_{nullptr};
+  atlas_serial_expander::AtlasSerialExpander *expander_{nullptr};
   uint8_t channel_{0};
 };
 
