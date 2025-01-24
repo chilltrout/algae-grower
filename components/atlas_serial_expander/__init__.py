5f18de95d62e339bb087a3316274bcf94ded4109
chilltrout <rbrown92@me.com>
2025-01-25 01:28:24 +1000

    Update __init__.py

--- components/atlas_serial_expander/__init__.py
+++ components/atlas_serial_expander/__init__.py
@@ -23,8 +23,8 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
 
-    s1 = await cg.gpio_pin_expression(config[CONF_S1_PIN])
-    s2 = await cg.gpio_pin_expression(config[CONF_S2_PIN])
-    s3 = await cg.gpio_pin_expression(config[CONF_S3_PIN])
+    s1 = await gpio.setup_gpio_output_pin(config['s1_pin'])
+    s2 = await gpio.setup_gpio_output_pin(config['s2_pin'])
+    s3 = await gpio.setup_gpio_output_pin(config['s3_pin'])
 
     cg.add(var.set_pins(s1, s2, s3))
