be55df27d2be384c07e3b95990759abac1d7268a
chilltrout <rbrown92@me.com>
2025-01-24 11:03:34 +1000

    Update turbidity_sensor.h

--- components/turbidity_sensor/turbidity_sensor.h
+++ components/turbidity_sensor/turbidity_sensor.h
@@ -1,29 +1,31 @@
+// TurbiditySensor External Component Implementation
 #include "esphome.h"
 
-class TurbiditySensor : public PollingComponent {
+class TurbiditySensor : public PollingComponent, public Sensor {
  public:
   TurbiditySensor(GPIOPin *s1, GPIOPin *s2, GPIOPin *s3, UARTComponent *uart, int channel)
       : PollingComponent(10000), s1_pin(s1), s2_pin(s2), s3_pin(s3), uart_bus(uart), channel(channel) {}
 
   void setup() override {
-    ESP_LOGCONFIG("TurbiditySensor", "Setting up Turbidity Sensor on channel %d", channel);
+    ESP_LOGCONFIG(TAG, "Setting up Turbidity Sensor on channel %d", channel);
+    s1_pin->setup();
+    s2_pin->setup();
+    s3_pin->setup();
   }
 
   void update() override {
-    // Set the channel on the serial port expander
-    s1_pin->digital_write((channel & 0x01) > 0);
-    s2_pin->digital_write((channel & 0x02) > 0);
-    s3_pin->digital_write((channel & 0x04) > 0);
+    // Select the channel using the GPIO pins
+    s1_pin->digital_write((channel & 0x01) != 0);
+    s2_pin->digital_write((channel & 0x02) != 0);
+    s3_pin->digital_write((channel & 0x04) != 0);
 
-    // Small delay to stabilize switching
-    delay(100);
+    delay(100);  // Allow the switching to stabilize
 
-    // Send the request command to the sensor
+    // Send the read command to the sensor
     uint8_t command[] = {0x18, 0x05, 0x00, 0x01, 0x0D};
     uart_bus->write_array(command, sizeof(command));
 
-    // Wait for a response
-    delay(100);
+    delay(100);  // Wait for the sensor to respond
 
     // Read the response
     std::vector<uint8_t> response;
@@ -31,13 +33,11 @@ class TurbiditySensor : public PollingComponent {
       response.push_back(uart_bus->read());
     }
 
-    // Validate and parse the response
     if (response.size() >= 5 && response[0] == 0x18 && response[1] == 0x05) {
-      // Extract the turbidity value (byte 3 as per the datasheet)
       float turbidity_value = static_cast<float>(response[3]);
       publish_state(turbidity_value);
     } else {
-      ESP_LOGW("TurbiditySensor", "Invalid response on channel %d", channel);
+      ESP_LOGW(TAG, "Invalid response from turbidity sensor on channel %d", channel);
       publish_state(NAN);
     }
   }
@@ -48,4 +48,7 @@ class TurbiditySensor : public PollingComponent {
   GPIOPin *s3_pin;
   UARTComponent *uart_bus;
   int channel;
+  static const char *const TAG;
 };
+
+const char *const TurbiditySensor::TAG = "TurbiditySensor";
