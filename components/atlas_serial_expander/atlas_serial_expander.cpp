1e0d707624bfb0a86d53698643ed629261327543
chilltrout <rbrown92@me.com>
2025-01-25 01:49:38 +1000

    Update atlas_serial_expander.cpp

--- components/atlas_serial_expander/atlas_serial_expander.cpp
+++ components/atlas_serial_expander/atlas_serial_expander.cpp
@@ -1,22 +1,28 @@
 #include "atlas_serial_expander.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace atlas_serial_expander {
 
+static const char *TAG = "atlas_serial_expander";
+
 void AtlasSerialExpander::setup() {
-  s1_pin_->setup();
-  s2_pin_->setup();
-  s3_pin_->setup();
+  // Setup code here
+  ESP_LOGD(TAG, "Setting up Atlas Serial Expander");
+  if (s1_pin_ != nullptr) s1_pin_->setup();
+  if (s2_pin_ != nullptr) s2_pin_->setup();
+  if (s3_pin_ != nullptr) s3_pin_->setup();
 }
 
 void AtlasSerialExpander::loop() {
-  // Any continuous operations if needed
+  // Loop code here (if needed)
 }
 
 void AtlasSerialExpander::select_channel(uint8_t channel) {
-  s1_pin_->digital_write(channel & 0x01);
-  s2_pin_->digital_write((channel >> 1) & 0x01);
-  s3_pin_->digital_write((channel >> 2) & 0x01);
+  ESP_LOGD(TAG, "Selecting channel %d", channel);
+  if (s1_pin_ != nullptr) s1_pin_->digital_write(channel & 0x01);
+  if (s2_pin_ != nullptr) s2_pin_->digital_write((channel >> 1) & 0x01);
+  if (s3_pin_ != nullptr) s3_pin_->digital_write((channel >> 2) & 0x01);
 }
 
 }  // namespace atlas_serial_expander
