6e7b1feae8582cb4b661301c746d135ed8f1c619
chilltrout <rbrown92@me.com>
2025-02-11 12:39:00 +1000

    Update sensor.py

--- components/turbidity_sensor/sensor.py
+++ components/turbidity_sensor/sensor.py
@@ -1,32 +1,45 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, uart
-from esphome.const import CONF_ID, CONF_CHANNEL
-from .. import atlas_serial_expander
+from esphome.const import CONF_ID, CONF_NAME, CONF_TYPE
 
-DEPENDENCIES = ['uart', 'atlas_serial_expander']
-AUTO_LOAD = ['sensor']
+DEPENDENCIES = ["uart"]
 
-CONF_EXPANDER_ID = 'expander_id'
+# Define TurbiditySensor component
+turbidity_ns = cg.esphome_ns.namespace("turbidity_sensor")
+TurbiditySensor = turbidity_ns.class_("TurbiditySensor", cg.PollingComponent, uart.UARTDevice)
 
-turbidity_sensor_ns = cg.esphome_ns.namespace('turbidity_sensor')
-TurbiditySensor = turbidity_sensor_ns.class_('TurbiditySensor', cg.PollingComponent, sensor.Sensor)
+# Configuration options
+CONF_UART_ID = "uart_id"
+CONF_EXPANDER_ID = "expander_id"
+CONF_CHANNEL = "channel"
 
-CONFIG_SCHEMA = sensor.sensor_schema().extend({
-    cv.GenerateID(): cv.declare_id(TurbiditySensor),
-    cv.Required(CONF_CHANNEL): cv.int_range(min=0, max=7),
-    cv.Required(CONF_EXPANDER_ID): cv.use_id(atlas_serial_expander.AtlasSerialExpander),
-    cv.Required('uart_id'): cv.use_id(uart.UARTComponent),
-}).extend(cv.polling_component_schema('60s'))
+# Supported types
+TYPE_OPTIONS = {
+    "turbidity": turbidity_ns.enum("TURBIDITY_TYPE_TURBIDITY"),
+    "adc": turbidity_ns.enum("TURBIDITY_TYPE_ADC"),
+}
 
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(TurbiditySensor),
+        cv.Required(CONF_NAME): cv.string,
+        cv.Required(CONF_UART_ID): cv.use_id(uart.UARTDevice),
+        cv.Required(CONF_EXPANDER_ID): cv.use_id(cg.global_ns.class_("AtlasSerialExpander")),
+        cv.Required(CONF_CHANNEL): cv.int_range(min=0, max=7),
+        cv.Required(CONF_TYPE): cv.enum(TYPE_OPTIONS, upper=True),
+    }
+).extend(cv.polling_component_schema()).extend(uart.UART_DEVICE_SCHEMA)
+
+# Build function to register sensors
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await sensor.register_sensor(var, config)
+    await uart.register_uart_device(var, config)
 
-    uart_component = await cg.get_variable(config['uart_id'])
-    cg.add(var.set_uart(uart_component))
-
-    expander = await cg.get_variable(config[CONF_EXPANDER_ID])
-    cg.add(var.set_expander(expander))
+    # Set parameters
+    cg.add(var.set_name(config[CONF_NAME]))
+    cg.add(var.set_uart_parent(await cg.get_variable(config[CONF_UART_ID])))
+    cg.add(var.set_expander_parent(await cg.get_variable(config[CONF_EXPANDER_ID])))
     cg.add(var.set_channel(config[CONF_CHANNEL]))
+    cg.add(var.set_type(config[CONF_TYPE]))
