7ff346145850a506133c57296300c9b951baca6d
chilltrout <rbrown92@me.com>
2025-02-11 15:05:58 +1000

    Update turbidity_sensor.h

--- components/turbidity_sensor/turbidity_sensor.h
+++ components/turbidity_sensor/turbidity_sensor.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/atlas_serial_expander/atlas_serial_expander.h"
 
 namespace esphome {
 namespace turbidity_sensor {
@@ -17,6 +18,8 @@ class TurbiditySensor : public PollingComponent, public uart::UARTDevice {
   void set_type(TurbiditySensorType type) { this->type_ = type; }
   void set_turbidity_sensor(sensor::Sensor *turbidity_sensor) { this->turbidity_sensor_ = turbidity_sensor; }
   void set_adc_sensor(sensor::Sensor *adc_sensor) { this->adc_sensor_ = adc_sensor; }
+  void set_expander_parent(esphome::atlas_serial_expander::AtlasSerialExpander* expander_parent) { this->expander_parent_ = expander_parent; }
+  void set_channel(uint8_t channel) { this->channel_ = channel; }
 
  protected:
   void update() override;
@@ -26,6 +29,8 @@ class TurbiditySensor : public PollingComponent, public uart::UARTDevice {
   TurbiditySensorType type_ = TurbiditySensorType::TURBIDITY;
   sensor::Sensor *turbidity_sensor_{nullptr};
   sensor::Sensor *adc_sensor_{nullptr};
+  esphome::atlas_serial_expander::AtlasSerialExpander* expander_parent_{nullptr};
+  uint8_t channel_{0};
   std::vector<uint8_t> rx_buffer_;
 };
 
