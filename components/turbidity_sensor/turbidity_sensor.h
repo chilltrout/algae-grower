f1cff897e0ef51a6453a2d11d0f7e99dc42071f4
chilltrout <rbrown92@me.com>
2025-02-11 17:00:17 +1000

    Update turbidity_sensor.h

--- components/turbidity_sensor/turbidity_sensor.h
+++ components/turbidity_sensor/turbidity_sensor.h
@@ -1,8 +1,8 @@
 #pragma once
-
 #include "esphome/core/component.h"
-#include "esphome/components/uart/uart.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/uart/uart.h"
+#include "esphome/core/polling_component.h"
 #include "esphome/components/atlas_serial_expander/atlas_serial_expander.h"
 
 namespace esphome {
@@ -13,29 +13,34 @@ enum class TurbiditySensorType {
   ADC,
 };
 
-class TurbiditySensor : public PollingComponent,  public sensor::Sensor {
- public:
-  TurbiditySensor() {}
-  void set_uart_parent(uart::UARTComponent *uart_parent) { this->uart_parent_ = uart_parent; }
-  void set_expander_parent(esphome::atlas_serial_expander::AtlasSerialExpander *expander_parent) { this->expander_parent_ = expander_parent; }
-  void set_channel(uint8_t channel) { this->channel_ = channel; }
-  void set_type(TurbiditySensorType type) { this->type_ = type; }
+struct TurbiditySensorConfig {
+  int channel;
+  TurbiditySensorType type;
+};
 
-  float get_setup_priority() const override { return setup_priority::DATA; }
+class TurbiditySensor : public PollingComponent, public sensor::Sensor {
+ public:
+  void set_channel(int channel) { channel_ = channel; }
+  int get_channel() const { return channel_; }
+
+  void set_type(TurbiditySensorType type) { type_ = type; }
+  TurbiditySensorType get_type() const { return type_; }
+
+  void set_uart_parent(uart::UARTComponent *parent) { uart_parent_ = parent; }
+  void set_expander(atlas_serial_expander::AtlasSerialExpander *expander) { expander_ = expander; }
+
+  void setup() override;
+  void update() override;
+  void dump_config() override;
 
  protected:
-  void update() override;
   void request_data_();
-  bool wait_for_response_();
-  bool parse_response_(float &value);
-  
+  void read_data_();
 
- private:
-  uart::UARTComponent *uart_parent_{nullptr};
-  esphome::atlas_serial_expander::AtlasSerialExpander *expander_parent_{nullptr};
-  uint8_t channel_{0};
+  int channel_{0};
   TurbiditySensorType type_{TurbiditySensorType::TURBIDITY};
-    std::vector<uint8_t> rx_buffer_;
+  uart::UARTComponent *uart_parent_{nullptr};
+  atlas_serial_expander::AtlasSerialExpander *expander_{nullptr};
 };
 
 }  // namespace turbidity_sensor
