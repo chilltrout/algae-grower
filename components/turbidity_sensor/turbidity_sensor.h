416566c2b418dbd790976e4d70f88a69769a85dc
chilltrout <rbrown92@me.com>
2025-02-11 12:15:43 +1000

    Update turbidity_sensor.h

--- components/turbidity_sensor/turbidity_sensor.h
+++ components/turbidity_sensor/turbidity_sensor.h
@@ -1,22 +1,26 @@
+// turbidity_sensor.h
 #pragma once
 
 #include "esphome.h"
+#include "atlas_serial_expander.h"
 
 namespace esphome {
 namespace turbidity_sensor {
 
-class TurbiditySensor : public Component, public UARTDevice {
+class TurbiditySensor : public PollingComponent, public sensor::Sensor {
  public:
-  TurbiditySensor(UARTComponent *parent) : UARTDevice(parent) {}
+  void setup() override;
+  void update() override;
+  void set_uart(UARTComponent *uart) { this->uart_ = uart; }
+  void set_expander(atlas_serial_expander::AtlasSerialExpander *expander) { this->expander_ = expander; }
+  void set_channel(uint8_t channel) { this->channel_ = channel; }
 
-  void set_turbidity_sensor(Sensor *sensor) { turbidity_sensor_ = sensor; }
-  void set_adc_sensor(Sensor *sensor) { adc_sensor_ = sensor; }
-
-  void read_sensors();
-
- protected:
-  Sensor *turbidity_sensor_{nullptr};
-  Sensor *adc_sensor_{nullptr};
+ private:
+  UARTComponent *uart_;
+  atlas_serial_expander::AtlasSerialExpander *expander_;
+  uint8_t channel_;
+  void request_data_();
+  bool parse_response_(const std::vector<uint8_t> &response, float &turbidity, float &adc_value);
 };
 
 }  // namespace turbidity_sensor
