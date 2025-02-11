5471ffe89b9b3f50c60cc11dde73d12f9a896de9
chilltrout <rbrown92@me.com>
2025-02-11 16:01:17 +1000

    Update sensor.py

--- components/turbidity_sensor/sensor.py
+++ components/turbidity_sensor/sensor.py
@@ -1,41 +1,66 @@
-#pragma once
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor, uart
+from esphome.const import CONF_ID, CONF_NAME, UNIT_EMPTY, ICON_EMPTY
 
-#include "esphome/core/component.h"
-#include "esphome/components/uart/uart.h"
-#include "esphome/components/sensor/sensor.h"
-#include "esphome/components/atlas_serial_expander/atlas_serial_expander.h"
+DEPENDENCIES = ["uart", "atlas_serial_expander"]
 
-namespace esphome {
-namespace turbidity_sensor {
+turbidity_ns = cg.esphome_ns.namespace("turbidity_sensor")
+TurbiditySensor = turbidity_ns.class_("TurbiditySensor", cg.PollingComponent, uart.UARTDevice)
 
-enum class TurbiditySensorType {
-  TURBIDITY,
-  ADC,
-};
+CONF_TURBIDITY_SENSOR = "turbidity_sensor"
+CONF_ADC_SENSOR = "adc_sensor"
+CONF_TYPE = "type"
+CONF_UART_ID = "uart_id"
+CONF_EXPANDER_ID = "expander_id"
+CONF_CHANNEL = "channel"
 
-class TurbiditySensor : public PollingComponent,  public sensor::Sensor {
- public:
-  void set_uart_parent(uart::UARTComponent *uart_parent) { this->uart_parent_ = uart_parent; }
-  void set_expander_parent(esphome::atlas_serial_expander::AtlasSerialExpander *expander_parent) { this->expander_parent_ = expander_parent; }
-  void set_channel(uint8_t channel) { this->channel_ = channel; }
-  void set_type(TurbiditySensorType type) { this->type_ = type; }
+TURBIDITY_SENSOR_TYPE = cv.enum({
+    "turbidity": 0,
+    "adc": 1,
+})
 
-  float get_setup_priority() const override { return setup_priority::DATA; }
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(TurbiditySensor),
+        cv.Required(CONF_NAME): cv.string,
+        cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
+        cv.Optional(CONF_EXPANDER_ID): cv.use_id(cg.Component),  # Assuming atlas_serial_expander is a Component
+        cv.Required(CONF_CHANNEL): cv.int_,
+        cv.Required(CONF_TYPE): TURBIDITY_SENSOR_TYPE,
+        cv.Optional(CONF_TURBIDITY_SENSOR): sensor.sensor_schema(
+            {
+                UNIT_EMPTY: UNIT_EMPTY,
+                ICON_EMPTY: ICON_EMPTY,
+            }
+        ),
+        cv.Optional(CONF_ADC_SENSOR): sensor.sensor_schema(
+            {
+                UNIT_EMPTY: UNIT_EMPTY,
+                ICON_EMPTY: ICON_EMPTY,
+            }
+        ),
+    }
+).extend(cv.polling_component_schema("60s"))
 
- protected:
-  void update() override;
-  void request_data_();
-  bool wait_for_response_();
-  bool parse_response_(float &value);
-  
 
- private:
-  uart::UARTComponent *uart_parent_{nullptr};
-  esphome::atlas_serial_expander::AtlasSerialExpander *expander_parent_{nullptr};
-  uint8_t channel_{0};
-  TurbiditySensorType type_{TurbiditySensorType::TURBIDITY};
-    std::vector<uint8_t> rx_buffer_;
-};
+async def to_code(config):
+  var = cg.new_Pvariable(config[CONF_ID], await cg.get_variable(config[CONF_UART_ID]))
+  await cg.register_component(var, config)
 
-}  // namespace turbidity_sensor
-}  // namespace esphome
+  uart_component = await cg.get_variable(config[CONF_UART_ID])
+  cg.add(var.set_uart_parent(uart_component))
+
+  if CONF_EXPANDER_ID in config:
+    expander_component = await cg.get_variable(config[CONF_EXPANDER_ID])
+    cg.add(var.set_expander_parent(expander_component))
+
+  cg.add(var.set_channel(config[CONF_CHANNEL]))
+
+  if config[CONF_TYPE] == 0:
+    cg.add(var.set_type(cg.RawExpression("turbidity_sensor::TurbiditySensorType::TURBIDITY")))
+    if CONF_TURBIDITY_SENSOR in config:
+      sens = await sensor.new_sensor(config[CONF_TURBIDITY_SENSOR])
+      cg.add(var.set_turbidity_sensor(sens))
+  else:
+    cg.add(var.set_type(cg.RawExpression("turbidity_sensor
