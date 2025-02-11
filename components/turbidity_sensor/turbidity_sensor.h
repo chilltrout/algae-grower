a4b4155d3f66b11aca7de2861eaf1075b0e5016f
chilltrout <rbrown92@me.com>
2025-02-11 15:57:10 +1000

    Update turbidity_sensor.h

--- components/turbidity_sensor/turbidity_sensor.h
+++ components/turbidity_sensor/turbidity_sensor.h
@@ -13,25 +13,28 @@ enum class TurbiditySensorType {
   ADC,
 };
 
-class TurbiditySensor : public PollingComponent, public sensor::Sensor {
+class TurbiditySensor : public PollingComponent,  public sensor::Sensor {
  public:
   void set_uart_parent(uart::UARTComponent *uart_parent) { this->uart_parent_ = uart_parent; }
   void set_expander_parent(esphome::atlas_serial_expander::AtlasSerialExpander *expander_parent) { this->expander_parent_ = expander_parent; }
   void set_channel(uint8_t channel) { this->channel_ = channel; }
   void set_type(TurbiditySensorType type) { this->type_ = type; }
 
+  float get_setup_priority() const override { return setup_priority::DATA; }
+
  protected:
   void update() override;
   void request_data_();
   bool wait_for_response_();
   bool parse_response_(float &value);
+  
 
  private:
   uart::UARTComponent *uart_parent_{nullptr};
   esphome::atlas_serial_expander::AtlasSerialExpander *expander_parent_{nullptr};
   uint8_t channel_{0};
   TurbiditySensorType type_{TurbiditySensorType::TURBIDITY};
-  std::vector<uint8_t> rx_buffer_;
+    std::vector<uint8_t> rx_buffer_;
 };
 
 }  // namespace turbidity_sensor
